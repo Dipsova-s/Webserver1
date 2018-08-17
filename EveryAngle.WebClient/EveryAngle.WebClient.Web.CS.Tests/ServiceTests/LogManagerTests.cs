3639160e8317b3991e7c3e8b01dd7c3e127c600c
Hero <kh@manaosoftware.com>
2018-08-17 15:28:05 +0700

    M4-47286: Anonymize passwords in log file from [PUT] password/changepassword

---
+++ EveryAngle.WebClient/EveryAngle.WebClient.Web.CS.Tests/ServiceTests/LogManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using EveryAngle.WebClient.Service.LogHandlers;
+using EveryAngle.WebClient.Web.CSTests.TestBase;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryAngle.WebClient.Web.CS.Tests.ServiceTests
+{
+    [TestFixture]
+    public class LogManagerTests : UnitTestBase
+    {
+        [TestCase("{\"username\":\"eaadmin\",\"oldpassword\":\"********\"}", "{\"username\":\"eaadmin\",\"oldpassword\":\"SpecialCharsTest!@#$%^&*()_+\"}")]
+        [TestCase("{\"username\":\"eaadmin\",\"newpassword\":\"********\"}", "{\"username\":\"eaadmin\",\"newpassword\":\"LetterWithNumericTest1234567890\"}")]
+        [TestCase("{\"username\":\"eaadmin\",\"oldpassword\":\"********\",\"newpassword\":\"********\"}", "{\"username\":\"eaadmin\",\"oldpassword\":\"VeryVeryLonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\",\"newpassword\":\"VeryVeryLonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\"}")]
+        public void Can_Anonymize_Password_Fields(string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging("password/changepassword", body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
