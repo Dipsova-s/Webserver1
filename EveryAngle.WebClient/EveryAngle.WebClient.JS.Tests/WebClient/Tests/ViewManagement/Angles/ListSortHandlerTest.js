006f8551020414ca4d343649ed249bc6cc370eb2
Sathit Uwan <su@manaosoftware.com>
2019-01-23 08:35:23 +0000

    Merged PR 1468: M4-48559: SD 1809 12377 / Sort indicator incorrectly set after sort limit warning
    fixed sorting icon + unit tests
    
    ![image.png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/1468/attachments/image.png)
    
    Cherry picked from !1410

---
+++ EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/WebClient/Tests/ViewManagement/Angles/ListSortHandlerTest.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="/Dependencies/ViewModels/Models/Angle/resultmodel.js" />
+/// <reference path="/Dependencies/ViewManagement/Shared/PopupPageHandlers.js" />
+/// <reference path="/Dependencies/ViewManagement/Angles/ListSortHandler.js" />
+
+describe("ListSortHandler", function () {
+
+    var listSortHandler;
+
+    beforeEach(function () {
+        listSortHandler = new ListSortHandler();
+    });
+
+    describe(".ApplyFail", function () {
+
+        it("should set event for popup.OnCloseCallback function", function (done) {
+            popup.OnCloseCallback = null;
+            listSortHandler.ApplyFail();
+
+            setTimeout(function () {
+                expect(popup.OnCloseCallback).not.toEqual(null);
+                expect(popup.OnCloseCallback.toString()).toContain('RollbackSorting');
+                done();
+            }, 200);
+        });
+
+    });
+
+    describe(".ApplyDone", function () {
+
+        it("should set event for popup.OnCloseCallback function if sorting limit exceeded", function () {
+            popup.OnCloseCallback = null;
+            spyOn(popup, 'Alert').and.callFake($.noop);
+            spyOn(resultModel, 'ApplyResult').and.callFake($.noop);
+            listSortHandler.ApplyDone(true);
+
+            expect(popup.Alert).toHaveBeenCalled();
+            expect(popup.OnCloseCallback).not.toEqual(null);
+            expect(popup.OnCloseCallback.toString()).toContain('RollbackSorting');
+            expect(resultModel.ApplyResult).not.toHaveBeenCalled();
+        });
+
+        it("should call ResultModel.ApplyResult function", function () {
+            spyOn(popup, 'Alert').and.callFake($.noop);
+            spyOn(resultModel, 'ApplyResult').and.callFake($.noop);
+            listSortHandler.ApplyDone(false);
+
+            expect(popup.Alert).not.toHaveBeenCalled();
+            expect(resultModel.ApplyResult).toHaveBeenCalled();
+        });
+
+    });
+});
