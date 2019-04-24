889abc6819c21f2edc00c6a790dc1a46bc05bab6
Hero <heroexe@hotmail.com>
2019-04-24 11:05:42 +0000

    Merged PR 2597: M4-55711: SD 1902 4071 Admin cannot unvalidate a Dashboard that was validated by another user
    ![image.png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/2576/attachments/image.png)
    
    all tests that related with WC are green
    
    Cherry picked from !2576

--- EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/WebClient/Tests/ViewManagement/Dashboard/DashboardDetailsHandlerTest.js
+++ EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/WebClient/Tests/ViewManagement/Dashboard/DashboardDetailsHandlerTest.js
@@ -344,25 +344,54 @@ describe("DashboardDetailsHandler", function () {
                 name: 'should no clean up input data if a user can update dashboard',
                 canUpdate: true,
                 input: { id: 'id1', user_specific: 'xx' },
-                expected: { id: 'id1', user_specific: 'xx' }
+                expected: { id: 'id1', user_specific: 'xx' },
+                dashboardData: function () {
+                    return {
+                        authorizations: {}
+                    };
+                }
+            },
+            {
+                name: 'should not delete is_validated property when a user unvalidateable dashboard even calculation of \"canUpdate\" is false',
+                canUpdate: false,
+                input: { is_validated: false },
+                expected: { is_validated: false },
+                dashboardData: function () {
+                    return {
+                        authorizations: {
+                            unvalidate: true
+                        }
+                    };
+                }
             },
             {
                 name: 'should clean up other properties than \"user_specific\" if a user cannot update dashboard #1',
                 canUpdate: false,
                 input: { id: 'id1', user_specific: 'xx' },
-                expected: { user_specific: 'xx' }
+                expected: { user_specific: 'xx' },
+                dashboardData: function () {
+                    return {
+                        authorizations: {}
+                    };
+                }
             },
             {
                 name: 'should clean up other properties than \"user_specific\" if a user cannot update dashboard #2',
                 canUpdate: false,
                 input: { id: 'id1' },
-                expected: {}
+                expected: {},
+                dashboardData: function () {
+                    return {
+                        authorizations: {}
+                    };
+                }
             }
         ];
 
         $.each(tests, function (index, test) {
             it(test.name, function () {
                 spyOn(dashboardModel, 'CanUpdateDashboard').and.returnValue(test.canUpdate);
+                spyOn(dashboardModel, 'Data').and.callFake(test.dashboardData);
 
                 dashboardDetailsHandler.CleanUpdatingData(test.input);
                 expect(JSON.stringify(test.expected)).toEqual(JSON.stringify(test.input));
