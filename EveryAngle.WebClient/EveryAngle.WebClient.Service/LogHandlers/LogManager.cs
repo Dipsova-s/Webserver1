2d768e353b1f87d0e55e2c21bd237f705ba05455
Nitish Maximus <nitish.vivian@everyangle.org>
2020-09-25 07:15:26 +0000

    Merged PR 6099: M4-82202: Updating of download settings logs plain text password in AS and IT MC
    Mask Passwords in Logs
    ![image.png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/6099/attachments/image.png)
    ![image (2).png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/6099/attachments/image%20(2).png)
    PBI for 1 failure already exist.
    https://jira.everyangle.com/browse/M4-84550

--- EveryAngle.WebClient/EveryAngle.WebClient.Service/LogHandlers/LogManager.cs
+++ EveryAngle.WebClient/EveryAngle.WebClient.Service/LogHandlers/LogManager.cs
@@ -1,4 +1,6 @@
 using EveryAngle.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -59,15 +61,17 @@ namespace EveryAngle.WebClient.Service.LogHandlers
 
             return requestInfo;
         }
+
         public static void WriteLogRequestContent(Method method, string uri, IRestRequest request)
         {
             IList<string> cookies = GetHeaderParameters<IRestRequest>(request, ParameterType.Cookie);
             IList<string> headers = GetHeaderParameters<IRestRequest>(request, ParameterType.HttpHeader);
             string body = GetHeaderBody(request);
-            
+
             DataTable requestInfo = LogManager.GenerateInfoTable(method.ToString(), uri, headers, cookies, body, "Request");
             Log.SendTable(LogMessageType.Green, string.Format("{0} => {1}", method.ToString(), uri), requestInfo);
         }
+
         public static void WriteLogResponseContent(Method method, string uri, IRestResponse currentResponseData)
         {
             IList<string> cookies = GetHeaderParameters<IRestResponse>(currentResponseData, ParameterType.Cookie);
@@ -79,6 +83,7 @@ namespace EveryAngle.WebClient.Service.LogHandlers
                 Log.SendTable(LogMessageType.Green, string.Format("{0} <= {1}", method.ToString(), uri), requestInfo);
             }
         }
+
         public static List<string> GetHeaderParameters<T>(T header, ParameterType parameterType)
         {
             List<string> results = new List<string>();
@@ -92,7 +97,7 @@ namespace EveryAngle.WebClient.Service.LogHandlers
                     {
                         if (para.Type == parameterType)
                         {
-                            results.Add(para.Name + ": " + (para.Value==null? "" : para.Value.ToString()));
+                            results.Add(para.Name + ": " + (para.Value == null ? "" : para.Value.ToString()));
                         }
                     }
                 }
@@ -116,6 +121,7 @@ namespace EveryAngle.WebClient.Service.LogHandlers
 
             return results;
         }
+
         public static string GetHeaderBody(IRestRequest request)
         {
             string body = string.Empty;
@@ -127,9 +133,10 @@ namespace EveryAngle.WebClient.Service.LogHandlers
                     body = para.Value.ToString();
                 }
             }
-            
+
             return ParseBodyContentLogging(request.Resource, body, request.Method);
         }
+
         public static void WriteExceptionLog(Exception ex)
         {
             string exMessage;
@@ -166,45 +173,220 @@ namespace EveryAngle.WebClient.Service.LogHandlers
 
             return targetFolder;
         }
+
         public static string ParseBodyContentLogging(string url, string body, Method method)
         {
             string loggingBody = body;
-            if (body.Contains("authorization"))
-            {
-                loggingBody = "{\"authorization\":\"xxxxxxxxxxxxxxxxx\"}";
-            }
-            else if (method == Method.PUT &&
-                url.Equals("password/changepassword"))
-            {
-                var separators = new List<string> { ":", "," };
-                loggingBody = AnonymizeField("oldpassword", separators, loggingBody);
-                loggingBody = AnonymizeField("newpassword", separators, loggingBody);
-            }
-            return loggingBody;
-        }
-        #endregion
 
-        #region Private Methods
-        private static string AnonymizeField(string fieldName, IEnumerable<string> fieldSeparators, string input)
-        {
-            if (input.Contains(fieldName))
+            if (string.IsNullOrEmpty(loggingBody)
+                || !(method == Method.POST || method == Method.PUT))
             {
-                const string findExpression = "\"{0}\"\\s*?{1}\\s*?\"[^>]*?\"";
-                const string replaceExpression = "\"{0}\"{1}\"********\"";
-                string output = input;
+                return loggingBody;
+            }
 
-                foreach (string fieldSeparator in fieldSeparators)
+            // Some json documents are structured in an key/values structure, these will be handled differently.
+            if (LoggerHelper.UrlsHavingSettingsList().Any(url.Contains))
+            {
+                if (LoggerHelper.TryGetMaskedSettingsJson(body, "setting_list", "id", out loggingBody, false, LoggerHelper.GetPasswordIdsInSettings()))
                 {
-                    var find = string.Format(findExpression, fieldName, fieldSeparator);
-                    var replace = string.Format(replaceExpression, fieldName, fieldSeparator);
-
-                    output = Regex.Replace(output, find, replace, RegexOptions.IgnoreCase);
+                    return loggingBody;
                 }
-                return output;
             }
-            return input;
+            else if (LoggerHelper.UrlsHavingArgumentsList().Any(url.Contains))
+            {
+                if (LoggerHelper.TryGetMaskedSettingsJson(body, "arguments", "name",out loggingBody, false, LoggerHelper.GetPasswordNamesInArguments()))
+                {
+                    return loggingBody;
+                }
+            }
+            else
+            {
+                if (LoggerHelper.TryGetMaskedJson(body, out loggingBody, false, LoggerHelper.GetKnownPasswordPropertyNames()))
+                {
+                    return loggingBody;
+                }
+            }
+
+            return $"Unable to parse body: {method} {url}";
         }
         #endregion
+
+        internal static class LoggerHelper
+        {
+            private const string MASKING_CHARACTERS = "**********";
+
+            public static string[] GetPasswordIdsInSettings()
+            {
+                return new string[] { "sap_password", "copy_smb_password", "copy_ftp_password", "sap_ddb_password", "connection_password", "database_manager_connection_string" };
+            }
+
+            public static string[] GetKnownPasswordPropertyNames()
+            {
+                return new string[] { "authorization", "oldpassword", "newpassword", "smtp_password" };
+            }
+
+            public static string[] GetPasswordNamesInArguments()
+            {
+                return new string[] { "password" };
+            }
+
+            public static string[] UrlsHavingSettingsList()
+            {
+                return new string[] { "/agent/download_settings", "/agent/modelserver_settings", "/system/datastores" };
+            }
+
+            public static string[] UrlsHavingArgumentsList()
+            {
+                return new string[] { "/tasks" };
+            }
+
+            public static bool TryGetMaskedJson(string json, out string maskedJson, bool indented, params string[] propertiesToMask)
+            {
+                maskedJson = null;
+
+                if (!IsValidForMasking(json, propertiesToMask))
+                {
+                    return false;
+                }
+
+                try
+                {
+                    maskedJson = GetMaskedJson(json, indented, propertiesToMask);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            public static bool TryGetMaskedSettingsJson(string json, string settingsSectionName, string findByProperty, out string maskedSettingsJson, bool indented, params string[] settingsToMask)
+            {
+                maskedSettingsJson = null;
+
+                if (!IsValidForMasking(json, settingsToMask))
+                {
+                    return false;
+                }
+
+                try
+                {
+                    maskedSettingsJson = GetMaskedSettings(json, settingsSectionName, findByProperty, indented, settingsToMask);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            private static Dictionary<string, string[]> GetOptionsForSubstringMasking()
+            {
+                // settingId: FindExpression, ReplaceExpression
+                return new Dictionary<string, string[]>
+                {
+                    { "database_manager_connection_string", new string[] { "(Password=)[^>]*(;)", "$1" + MASKING_CHARACTERS + "$2" } }
+                };
+            }
+
+            private static bool IsValidForMasking(string json, params string[] maskingFields)
+            {
+                return !string.IsNullOrEmpty(json) && maskingFields != null && maskingFields.Any();
+            }
+
+            private static string GetMaskedSettings(string json, string settingsSectionName, string findByProperty, bool indented, params string[] settingsToMask)
+            {
+                var substringMaskingOptions = GetOptionsForSubstringMasking();
+                var templateSettings = JToken.Parse(json);
+
+                // based on the TemplateSettings json structure
+                var settingList = FindMatchingTokens(templateSettings, settingsSectionName);
+
+                var settings = settingList?.Children();
+
+                var matchingSettings = settings?.Where(o => o[findByProperty] != null &&
+                    settingsToMask.Contains(o[findByProperty].ToString(), StringComparer.OrdinalIgnoreCase));
+
+                if (!matchingSettings.Any())
+                {
+                    return json;
+                }
+
+                foreach (var matchingSetting in matchingSettings)
+                {
+                    if (substringMaskingOptions.ContainsKey(matchingSetting[findByProperty].ToString()))
+                    {
+                        string id = matchingSetting[findByProperty].ToString();
+                        string value = matchingSetting["value"].ToString();
+
+                        string findExpression = substringMaskingOptions[id][0];
+                        string replaceExpression = substringMaskingOptions[id][1];
+
+                        string maskedValue = Regex.Replace(value, findExpression, replaceExpression, RegexOptions.IgnoreCase);
+
+                        matchingSetting["value"] = maskedValue;
+                    }
+                    else
+                    {
+                        matchingSetting["value"] = MASKING_CHARACTERS;
+                    }
+                }
+
+                return JsonConvert.SerializeObject(templateSettings,
+                    indented ? Formatting.Indented : Formatting.None);
+            }
+
+            private static string GetMaskedJson(string json, bool indented, params string[] propertiesToMask)
+            {
+                var token = JToken.Parse(json);
+
+                var matchingTokens = FindMatchingTokens(token, propertiesToMask);
+
+                foreach (var matchingToken in matchingTokens)
+                {
+                    matchingToken.Replace(new JValue(MASKING_CHARACTERS));
+                }
+
+                return JsonConvert.SerializeObject(token, indented ? Formatting.Indented : Formatting.None);
+            }
+
+            private static IList<JToken> FindMatchingTokens(JToken containerToken, params string[] names)
+            {
+                var foundTokens = new List<JToken>();
+
+                if (!names.Any())
+                {
+                    return foundTokens;
+                }
+
+                if (containerToken.Type != JTokenType.Object)
+                {
+                    if (containerToken.Type != JTokenType.Array)
+                    {
+                        return foundTokens;
+                    }
+
+                    foreach (var child in containerToken.Children())
+                    {
+                        foundTokens.AddRange(FindMatchingTokens(child, names));
+                    }
+                }
+                else
+                {
+                    foreach (var child in containerToken.Children<JProperty>())
+                    {
+                        if (names.Contains(child.Name, StringComparer.OrdinalIgnoreCase))
+                        {
+                            foundTokens.Add(child.Value);
+                        }
+
+                        foundTokens.AddRange(FindMatchingTokens(child.Value, names));
+                    }
+                }
+
+                return foundTokens;
+            }
+        }
     }
     #endregion
 }
--- EveryAngle.WebClient/EveryAngle.WebClient.Web.CS.Tests/ServiceTests/LogManagerTests.cs
+++ EveryAngle.WebClient/EveryAngle.WebClient.Web.CS.Tests/ServiceTests/LogManagerTests.cs
@@ -2,23 +2,62 @@
 using EveryAngle.WebClient.Web.CSTests.TestBase;
 using NUnit.Framework;
 using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EveryAngle.WebClient.Web.CSTests.ServiceTests
 {
     [TestFixture]
     public class LogManagerTests : UnitTestBase
     {
-        [TestCase("{\"username\":\"eaadmin\",\"oldpassword\":\"********\"}", "{\"username\":\"eaadmin\",\"oldpassword\":\"SpecialCharsTest!@#$%^&*()_+\"}")]
-        [TestCase("{\"username\":\"eaadmin\",\"newpassword\":\"********\"}", "{\"username\":\"eaadmin\",\"newpassword\":\"LetterWithNumericTest1234567890\"}")]
-        [TestCase("{\"username\":\"eaadmin\",\"oldpassword\":\"********\",\"newpassword\":\"********\"}", "{\"username\":\"eaadmin\",\"oldpassword\":\"VeryVeryLonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\",\"newpassword\":\"VeryVeryLonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\"}")]
-        public void Can_Anonymize_Password_Fields(string expectedResult, string body)
+        [TestCase("password/changepassword", "{\"username\":\"eaadmin\",\"oldpassword\":\"**********\"}", "{\"username\":\"eaadmin\",\"oldpassword\":\"SpecialCharsTest!@#$%^&*()_+\"}")]
+        [TestCase("password/changepassword", "{\"username\":\"eaadmin\",\"newpassword\":\"**********\"}", "{\"username\":\"eaadmin\",\"newpassword\":\"LetterWithNumericTest1234567890\"}")]
+        [TestCase("/everyangle/randomUrl", "{\"username\":\"eaadmin\",\"oldpassword\":\"**********\",\"newpassword\":\"**********\"}", "{\"username\":\"eaadmin\",\"oldpassword\":\"VeryVeryLonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\",\"newpassword\":\"VeryVeryLonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\"}")]
+        public void Can_Anonymize_Password_Properties(string url, string expectedResult, string body)
         {
-            string actualResult = LogManager.ParseBodyContentLogging("password/changepassword", body, Method.PUT);
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("/agent/download_settings", "{\"setting_list\":[{\"id\":\"copy_smb_username\",\"value\":\"sapuser\"},{\"id\":\"copy_smb_password\",\"value\":\"**********\"}]}", "{\"setting_list\":[{\"id\":\"copy_smb_username\",\"value\":\"sapuser\"},{\"id\":\"copy_smb_password\",\"value\":\"SpecialCharsTest!@#$%^&*()_+\"}]}")]
+        [TestCase("/agent/download_settings", "{\"setting_list\":[{\"id\":\"sap_username\",\"value\":\"sapuser\"},{\"id\":\"sap_password\",\"value\":\"**********\"}]}", "{\"setting_list\":[{\"id\":\"sap_username\",\"value\":\"sapuser\"},{\"id\":\"sap_password\",\"value\":\"LetterWithNumericTest1234567890\"}]}")]
+        public void Can_Anonymize_Password_Settings_KnownURL(string url, string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("/tasks", "{\"arguments\":[{\"name\":\"run_as_user\",\"value\":\"tester_1\"},{\"name\":\"password\",\"value\":\"**********\"}]}", "{\"arguments\":[{\"name\":\"run_as_user\",\"value\":\"tester_1\"},{\"name\":\"password\",\"value\":\"SpecialCharsTest!@#$%^&*()_+\"}]}")]
+        [TestCase("/tasks/99999", "{\"arguments\":[{\"name\":\"run_as_user\",\"value\":\"tester_2\"},{\"name\":\"password\",\"value\":\"**********\"}]}", "{\"arguments\":[{\"name\":\"run_as_user\",\"value\":\"tester_2\"},{\"name\":\"password\",\"value\":\"LetterWithNumericTest1234567890\"}]}")]
+        public void Can_Anonymize_Password_Names_KnownURL(string url, string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("/agent/modelserver_settings", "{\"setting_list\":[{\"id\":\"database_manager_connection_string\",\"value\":\"Server=nl-test.eatestad.local;Database=DB_Name;User Id=admin;Password=**********;MultipleActiveResultSets=true\"}]}", "{\"setting_list\":[{\"id\":\"database_manager_connection_string\",\"value\":\"Server=nl-test.eatestad.local;Database=DB_Name;User Id=admin;Password=SpecialCharsTest!@#$%^&*()_+;MultipleActiveResultSets=true\"}]}")]
+        public void Can_Anonymize_SubstringPassword_Settings(string url, string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("/everyangle/randomUrl", "{\"setting_list\":[{\"id\":\"username\",\"value\":\"sapuser\"},{\"id\":\"sap_password\",\"value\":\"LetterWithNumericTest1234567890\"}]}", "{\"setting_list\":[{\"id\":\"username\",\"value\":\"sapuser\"},{\"id\":\"sap_password\",\"value\":\"LetterWithNumericTest1234567890\"}]}")]
+        public void Get_Unchanged_Body_WhenUnknownURL(string url, string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("/agent/modelserver_settings", "{\"setting_list\":[{\"id\":\"username\",\"value\":\"user\"},{\"id\":\"dummy_password\",\"value\":\"LetterWithNumericTest1234567890\"}]}", "{\"setting_list\":[{\"id\":\"username\",\"value\":\"user\"},{\"id\":\"dummy_password\",\"value\":\"LetterWithNumericTest1234567890\"}]}")]
+        public void Get_Unchanged_Body_WhenNoMatchingSettings(string url, string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("/agent/modelserver_settings", "Unable to parse body: PUT /agent/modelserver_settings", "{\"setting_list\":[{\"id\":\"username\",\"value:\"user\"},{\"id\":\"dummy_password\",\"value\":\"LetterWithNumericTest1234567890\"}]}")]
+        public void Get_ErrorMessage_Body_WhenInvalidBody(string url, string expectedResult, string body)
+        {
+            string actualResult = LogManager.ParseBodyContentLogging(url, body, Method.PUT);
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
