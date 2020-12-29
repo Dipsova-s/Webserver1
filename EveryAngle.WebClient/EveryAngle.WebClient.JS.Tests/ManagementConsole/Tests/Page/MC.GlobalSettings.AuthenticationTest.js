271b88606ae24e9893bdf7a8c69fd2f9c97eca98
Dhananjaya Poojari <dhanajaya.poojari@everyangle.org>
2020-12-29 09:30:36 +0000

    Merged PR 6642: Pull Request 6633: M4-89344 Improvement of Authentication Automation - ITMC
    Pull Request 6633: M4-89344 Improvement of Authentication Automation - ITMC
    ![image.png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/6642/attachments/image.png)

---
+++ EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/ManagementConsole/Tests/Page/MC.GlobalSettings.AuthenticationTest.js
@@ -0,0 +1,29 @@
+﻿/// <chutzpah_reference path="/../../Dependencies/page/MC.GlobalSettings.Authentication.js" />
+/// <chutzpah_reference path="/../../Dependencies/custom/MC.form.js" />
+/// <chutzpah_reference path="/../../Dependencies/custom/MC.js" />
+
+describe("MC.GlobalSettings.Authentication", function () {
+    var authentication;
+    beforeEach(function () {
+        authentication = MC.GlobalSettings.Authentication;
+    })
+    describe(".GetData", function () {
+        var html;
+        beforeEach(function () {
+            html = $('<input  id="ReFormatTrustedWebservers" name="ReFormatTrustedWebservers" type="text" value="127.0.0.1, ::1, 10.64.96.12" >');
+            html.appendTo('body');
+        });
+        afterEach(function () {
+            html.remove();
+        });
+        it("Always return Ip address in the textbox", function () {
+            var expected = [
+                "127.0.0.1",
+                " ::1",
+                " 10.64.96.12"
+            ];
+            var result = authentication.GetData();
+            expect(result.systemSettings.trusted_webservers).toEqual(expected);
+        });
+    });
+});
--- Robot/WC/TC_MC_Authentication.robot
+++ Robot/WC/TC_MC_Authentication.robot
@@ -19,11 +19,3 @@ Test Authentication
     Save Authenticate And Invalid Trusted Webservers Should Be Cleared
     Check Automatically create users for "ADFS" is ON and disabled
     Check default Is Set Local    local
-
-Test Add New Authentication
-    [Tags]      TC_C14       TC_C15
-    [Documentation]     This test add new authentication, verify and then delete the authentication
-    ...                 Risk covered: This test covers the failures in the authentication module with all the possibilities 
-    Add New Authentication    Active Directory Federation Services Authentication Provider    ${AUTHENTICATION_PROVIDER}    test    ${DEFAULT_ROLE}    https://adfs.everyangle.com/adfs/services/trust/13/usernamemixed    https://ea2auth.everyangle.com/
-    Verify New Authentication    ${AUTHENTICATION_PROVIDER}
-    Delete Authentication Provider by ID    ${AUTHENTICATION_PROVIDER}
