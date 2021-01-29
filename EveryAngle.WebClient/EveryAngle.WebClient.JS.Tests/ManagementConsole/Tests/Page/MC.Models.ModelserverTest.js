f0eefc38658e11c6288765c268baf8dd6daed2f1
Dhananjaya Poojari <dhanajaya.poojari@everyangle.org>
2021-01-29 10:21:08 +0000

    Merged PR 6796: M4-90022 Improvement of Server settings: EA ETL Sandbox -Jasmine Test
    M4-90022 Improvement of Server settings: EA ETL Sandbox Automation-Jasmine Test

---
+++ EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/ManagementConsole/Tests/Page/MC.Models.ModelserverTest.js
@@ -0,0 +1,62 @@
+﻿/// <chutzpah_reference path="/../../Dependencies/custom/MC.js" />
+/// <chutzpah_reference path="/../../Dependencies/custom/MC.form.js" />
+/// <chutzpah_reference path="/../../Dependencies/page/MC.Models.Modelserver.js" />
+
+describe("MC.Models.Modelserver", function () {
+
+    let modelServer, domElement;
+    beforeEach(function () {
+        modelServer = MC.Models.ModelServer;
+        domElement = $('<div class="content pageModelContentParameters">' +
+            '<div class="contentSection contentSectionInfo sap_settings">' +
+            '<h2 style="cursor: pointer;" class="click" onclick="MC.Models.ModelServer.CollapsibleExpandable(this);"><span class="k-icon k-i-expand">SAP connection settings</span></h2>' +
+            '<form id="sap_settings" data-role="validator" autocomplete="off" style="display: none;" novalidate="novalidate" _lpchecked="1">' +
+            '<div class="contentSectionInfoItem" id="row-sap_host" style="padding-left: 20px;">' +
+            '<label data-role="tooltip">Hostname</label>' +
+            '<p><input class="text autosyncinput" data-setting-type="text" data-type="textbox" id="sap_host" name="sap_host" type="text" value="sapides720"></p>' +
+            '</form></div></div>');
+
+        domElement.appendTo('body');
+    });
+    afterEach(function () {
+        domElement.remove();
+    });
+
+    describe(".CollapsibleExpandable", function () {
+
+        it("Form should be visible when click on the header element", function () {
+            //Act
+            jQuery('.click').trigger('click');
+
+            //Assert
+            expect($("#sap_settings").is(':visible')).toBeTruthy();
+        });
+        it("Form should be hidden when click on the header element", function () {
+            //Act
+            jQuery('.click').trigger('click');
+            jQuery('.click').trigger('click');
+
+            //Assert
+            expect($("#sap_settings").is(':visible')).toBeFalsy();
+        });
+
+    });
+
+    describe(".GetData", function () {
+
+        it("Should return data from the form", function () {
+            //Prepare
+            const expected = {
+                id: "sap_host",
+                value: "sapides720",
+                type: "text"               
+            };
+
+            //Act
+            const result = modelServer.GetData();
+
+            //Assert
+            expect(result.settingList[0]).toEqual(expected);
+        });
+    });
+});
