aef8ef837d805eda444583412f2020413a89a14a
Geoes Shaji <gshaji@everyangle.org>
2021-02-01 06:39:02 +0000

    Merged PR 6807: M4-90023 Improvement of Server settings: Communications Automation
    Improvement of EA2 800 Automation
    
    ![image.png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/6807/attachments/image.png)

--- EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/ManagementConsole/Tests/Page/MC.Models.CommunicationsTest.js
+++ EveryAngle.WebClient/EveryAngle.WebClient.JS.Tests/ManagementConsole/Tests/Page/MC.Models.CommunicationsTest.js
@@ -14,12 +14,22 @@ describe("MC.Models.Communications", function () {
     });
 
     describe("when SaveModelCommunication without 'send_logs_frequency'", function () {
-
-        beforeEach(function () {
-            spyOn($.fn, 'val').and.returnValue("");
+        it(".GetData() should return values", function () {
+            var contact = 12345, address = "Company Address", city = "City", country = "Country", telephone = 12345678, email = "abc@xyz.com", sender = "xyz@srs.com", recipients = "abc@xyz.com,pqr@xyz.com", logFrequency = 2;
+            spyOn($.fn, 'val').and.returnValues(contact, address, city, country, telephone, email, sender, recipients, logFrequency);
+            var returnResult = communications.GetData();
+            expect(returnResult.companyInformationData.contact).toEqual(contact);
+            expect(returnResult.companyInformationData.address).toEqual(address);
+            expect(returnResult.companyInformationData.city).toEqual(city);
+            expect(returnResult.companyInformationData.country).toEqual(country);
+            expect(returnResult.companyInformationData.telephone).toEqual(telephone);
+            expect(returnResult.companyInformationData.email).toEqual(email);
+            expect(returnResult.emailSettingsData.sender).toEqual(sender);
+            expect(returnResult.emailSettingsData.recipients).toEqual(recipients.split(','));
+            expect(returnResult.emailSettingsData.send_logs_frequency).toEqual(logFrequency);
         });
-
         it(".GetData() should set default value to '0'", function () {
+            spyOn($.fn, 'val').and.returnValue("");
             var returnResult = communications.GetData();
             expect(returnResult.emailSettingsData.send_logs_frequency).toEqual(0);
         });
