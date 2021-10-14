ad3a2951316552f239684fb10ff4510cb875e75f
Lalit Negi <LNegi@everyangle.org>
2021-10-14 16:57:35 +0530

    M4-92711_Included "SAP download maximum runtime" field in Refresh cycle page

--- EveryAngle.WebClient/EveryAngle.Shared.Globalization/Resource.Designer.cs
+++ EveryAngle.WebClient/EveryAngle.Shared.Globalization/Resource.Designer.cs
@@ -17089,6 +17089,15 @@ namespace EveryAngle.Shared.Globalization {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to SAP download maximum run time.
+        /// </summary>
+        public static string SAPDownloadMaxRunTime {
+            get {
+                return ResourceManager.GetString("SAPDownloadMaxRunTime", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to SAP system.
         /// </summary>
         public static string SAPSystem {
