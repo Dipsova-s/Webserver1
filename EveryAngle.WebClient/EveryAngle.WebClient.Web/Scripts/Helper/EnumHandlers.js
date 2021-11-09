e8075316de3ef447f741d79923ed502ac965d1ec
Manisha Rana <mrana@everyangle.org>
2021-11-09 17:25:53 +0530

    Removed relative filter from date

--- EveryAngle.WebClient/EveryAngle.WebClient.Web/Scripts/Helper/EnumHandlers.js
+++ EveryAngle.WebClient/EveryAngle.WebClient.Web/Scripts/Helper/EnumHandlers.js
@@ -701,9 +701,6 @@ function EnumHandlers() {
             { Id: '', Value: self.OPERATOR.BEFORE.Value, Text: self.OPERATOR.BEFORE.Text },
             { Id: '', Value: self.OPERATOR.AFTER.Value, Text: self.OPERATOR.AFTER.Text },
             { Id: '', Value: self.OPERATOR.BETWEEN.Value, Text: self.OPERATOR.BETWEEN.Text },
-            { Id: '', Value: self.OPERATOR.RELATIVEBEFORE.Value, Text: self.OPERATOR.RELATIVEBEFORE.Text },
-            { Id: '', Value: self.OPERATOR.RELATIVEAFTER.Value, Text: self.OPERATOR.RELATIVEAFTER.Text },
-            { Id: '', Value: self.OPERATOR.RELATIVEBETWEEN.Value, Text: self.OPERATOR.RELATIVEBETWEEN.Text },
             { Id: '', Value: self.OPERATOR.HASNOVALUE.Value, Text: self.OPERATOR.HASNOVALUE.Text },
             { Id: '', Value: self.OPERATOR.HASVALUE.Value, Text: self.OPERATOR.HASVALUE.Text },
             { Id: '', Value: self.OPERATOR.NOTEQUALTO.Value, Text: self.OPERATOR.NOTEQUALTO.Text },
@@ -712,7 +709,6 @@ function EnumHandlers() {
             { Id: '', Value: self.OPERATOR.INLIST.Value, Text: self.OPERATOR.INLIST.Text },
             { Id: '', Value: self.OPERATOR.NOTINLIST.Value, Text: self.OPERATOR.NOTINLIST.Text },
             { Id: '', Value: self.OPERATOR.NOTBETWEEN.Value, Text: self.OPERATOR.NOTBETWEEN.Text },
-            { Id: '', Value: self.OPERATOR.NOTRELATIVEBETWEEN.Value, Text: self.OPERATOR.NOTRELATIVEBETWEEN.Text }
         ]
 
     };
