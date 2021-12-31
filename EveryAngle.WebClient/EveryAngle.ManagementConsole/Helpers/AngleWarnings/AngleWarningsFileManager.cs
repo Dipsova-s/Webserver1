d7cc6dc4d86218f1c970c84451bc11af0eec67d9
Lalit Negi <LNegi@everyangle.org>
2021-12-31 16:01:14 +0530

    Fixed code smell

--- EveryAngle.WebClient/EveryAngle.ManagementConsole/Helpers/AngleWarnings/AngleWarningsFileManager.cs
+++ EveryAngle.WebClient/EveryAngle.ManagementConsole/Helpers/AngleWarnings/AngleWarningsFileManager.cs
@@ -71,7 +71,7 @@ namespace EveryAngle.ManagementConsole.Helpers.AngleWarnings
 
             if (file is null)
             {
-                throw new ArgumentNullException("Setting AngleWarningsContentInputFile not found.", nameof(file));
+                throw new ArgumentNullException(nameof(file), "Setting AngleWarningsContentInputFile not found.");
             }
 
             if (!_fileHelper.FileExists(file))
