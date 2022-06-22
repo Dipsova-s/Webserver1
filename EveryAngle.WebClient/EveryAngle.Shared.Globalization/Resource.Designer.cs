386ab472026815e12e2eb14aff27a7c4189a5001
Vrushali Kulkarni <vkulkarni@everyangle.org>
2022-06-22 06:33:17 +0000

    Merged PR 9249: M4-103196 Updated label of "External id and is available externally flag"
    Updated label and Moved to Resource.resx file
    ![image (3).png](https://everyangle.visualstudio.com/944c3508-25d4-46f4-ad5f-72eb92421f4a/_apis/git/repositories/5d6d255e-4f64-402e-83ea-bdae40b79152/pullRequests/9249/attachments/image%20%283%29.png)

--- EveryAngle.WebClient/EveryAngle.Shared.Globalization/Resource.Designer.cs
+++ EveryAngle.WebClient/EveryAngle.Shared.Globalization/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace EveryAngle.Shared.Globalization {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -10622,6 +10622,15 @@ namespace EveryAngle.Shared.Globalization {
         public static string MC_IncludedLabels {
             get {
                 return ResourceManager.GetString("MC_IncludedLabels", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Include Available via Gateway settings.
+        /// </summary>
+        public static string MC_IncludeIdsForExternalAvailability {
+            get {
+                return ResourceManager.GetString("MC_IncludeIdsForExternalAvailability", resourceCulture);
             }
         }
         
